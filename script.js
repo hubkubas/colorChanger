58bf2fbb0503224b0b96e09e000211ccc22b20eb
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2018-08-27 22:55:42 +0200

    The last version

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
 var button = document.querySelector("button");
-// var isPurple = false;
 
+function myFunction(){
 
+document.body.classList.toggle ("purple");
 
-button.addEventListener("click", function(){
-
-  document.body.classList.toggle ("purple");
-
-
-});
-
-
-// button.addEventListener("click", function(){
-//
-//   if(isPurple){
-//   document.body.style.background = "white";
-//   isPurple = false;
-// } else {
-//   document.body.style.background = "purple";
-//   isPurple = true;
-// }
-// });
-
-
-// var button = document.querySelector("button");
-//
-// button.addEventListener("click", function(){
-//   // alert("przycisk klinknięty");
-//   document.body.style.backgroundColor = "pink";
-// })
+};
