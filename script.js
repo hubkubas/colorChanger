9edc2a8a6e6be551c6a014aff553b8a843042009
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2018-08-29 15:35:36 +0200

    FINAL VERSION
    The simplest and the final version :)

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-
-var tlo = document.getElementById("box");
+var background = document.getElementById("box");
 
 var colorChanger = {
-
-  reset: function() {
-    if ((tlo.classList.contains("white")) || (tlo.classList.contains("blue")) || (tlo.classList.contains("purple"))){
-      tlo.classList.remove("blue") || tlo.classList.remove("purple") || tlo.classList.remove("white");
-      console.log("No color");
-    } else {
-      console.log("brak koloru");
-    }
-  },
-
-  white: function() {
-    tlo.classList.add("white");
-    console.log("white");
-  },
-  purple: function() {
-    tlo.classList.add("purple");
-    console.log("purple");
-  },
-  blue: function() {
-    tlo.classList.add("blue");
-    console.log("blue");
+  changeTo: function(newClass) {
+    background.className = newClass;
   }
 };
