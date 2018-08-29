8825a1ac4d24ba136a7dc06a474d8f3d68d9a717
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2018-08-29 22:52:55 +0200

    Revert "FINAL VERSION"
    This reverts commit 9edc2a8a6e6be551c6a014aff553b8a843042009.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var background = document.getElementById("box");
+
+var tlo = document.getElementById("box");
 
 var colorChanger = {
-  changeTo: function(newClass) {
-    background.className = newClass;
+
+  reset: function() {
+    if ((tlo.classList.contains("white")) || (tlo.classList.contains("blue")) || (tlo.classList.contains("purple"))){
+      tlo.classList.remove("blue") || tlo.classList.remove("purple") || tlo.classList.remove("white");
+      console.log("No color");
+    } else {
+      console.log("brak koloru");
+    }
+  },
+
+  white: function() {
+    tlo.classList.add("white");
+    console.log("white");
+  },
+  purple: function() {
+    tlo.classList.add("purple");
+    console.log("purple");
+  },
+  blue: function() {
+    tlo.classList.add("blue");
+    console.log("blue");
   }
 };
