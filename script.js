22e3ad8a51379f9fc9ccab44ab0da828cc6d52ae
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2018-08-29 13:06:48 +0200

    New version
    With reset button

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 
-function purpleFunction(){
-document.body.classList.toggle ("purple");
-};
+var tlo = document.getElementById("box");
 
-function blueFunction(){
-document.body.classList.toggle ("blue");
+var colorChanger = {
+
+  reset: function() {
+    if ((tlo.classList.contains("white")) || (tlo.classList.contains("blue")) || (tlo.classList.contains("purple"))){
+      tlo.classList.remove("blue") || tlo.classList.remove("purple") || tlo.classList.remove("white");
+      console.log("No color");
+    } else {
+      console.log("brak koloru");
+    }
+  },
+
+  white: function() {
+    tlo.classList.add("white");
+    console.log("white");
+  },
+  purple: function() {
+    tlo.classList.add("purple");
+    console.log("purple");
+  },
+  blue: function() {
+    tlo.classList.add("blue");
+    console.log("blue");
+  }
 };
